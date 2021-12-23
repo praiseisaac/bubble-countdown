ee02ae9e25d873b87ba576bf3ab6b1da986959c8
praise <daramolapraise@gmail.com>
2021-12-23 16:36:45 -0500

    initial

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ const clear = () => {
   clearInterval(timeoutId)
 }
 
-const set = (stopTime) => {
+const set = (stopTime = "Thu Dec 23 2021 16:35:31 GMT-0500 (Eastern Standard Time)") => {
   if (!stopTime) return;
   timeoutId = setInterval(() => {
-    if ((new Date(Date.now())) > new Date(stopTime)) {
+    var timeDiff = ((new Date(stopTime)) - (new Date(Date.now()))) / 1000;
+    if (timeDiff <= 0) {
       clear();
     }
-    console.log((new Date(Date.now())).toLocaleTimeString());
+    console.log(convert(timeDiff));
     count++;
   }, 1000);
 }
@@ -23,5 +24,17 @@ const set = (stopTime) => {
 const reset = () => {
   clear();
   set();
+}
+
+var convert = (seconds) => {
+  var secs = parseInt(seconds, 10);
+  var h = Math.floor(secs / 3600);
+  var m = Math.floor(secs / 60) % 60;
+  var s = secs % 60
+
+  return [h, m, s]
+    .map(val => val < 10 ? "0" + val : val)
+    .filter((val, ix) => val !== "00" || ix > 0)
+    .join(":");
 }
 
